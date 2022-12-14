b05ffab58acc765597fba1e9dcb05bb9a3a22253
Halil Ücel <hucelsoft@gmail.com>
2022-12-14 15:33:01 +0300

    test widget

---
+++ lib/screen/authentication/forgot/forgot_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:hucel_core/hucel_core.dart';
+
+class ForgotScreen extends StatelessWidget {
+  const ForgotScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: CenterContainer(child: Text("Hello Github")),
+    );
+  }
+}
