b18d541d4e5ea7c2466fe09abbf29ca01ce4912c
Halil Ücel <hucelsoft@gmail.com>
2022-12-27 16:19:30 +0300

    firebase add ımage

--- lib/screen/page/home/home_screen.dart
+++ lib/screen/page/home/home_screen.dart
@@ -4,6 +4,9 @@ import 'package:hucel_core/hucel_core.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  final String background =
+      "https://firebasestorage.googleapis.com/v0/b/code-of-land.appspot.com/o/assets%2Fimage%2Fwelcome_background.jpg?alt=media&token=bcef1011-2f6e-4f0d-8adf-e99979506289";
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -25,25 +28,55 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
+  // Widget _welcomePage(context) {
+  //   return MultiBuilder(
+  //       builder: (context, height, width, constrain, orientation) {
+  //     return SizedBox(
+  //       height: constrain.maxWidth > 800 ? constrain.maxWidth * 0.6 : 480,
+  //       child: Column(
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         children: [
+  //           const Expanded(
+  //               child: Image(
+  //                   image: NetworkImage('https://picsum.photos/600/360'))),
+  //           Text(
+  //             'Hello, Welcome to my site, I will help you for better than development on flutter dart languages.',
+  //             style: context.headline5,
+  //             textAlign: TextAlign.center,
+  //           )
+  //         ],
+  //       ),
+  //     );
+  //   });
+  // }
+
   Widget _welcomePage(context) {
-    return MultiBuilder(
-        builder: (context, height, width, constrain, orientation) {
-      return SizedBox(
-        height: constrain.maxWidth > 800 ? constrain.maxWidth * 0.6 : 480,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Expanded(
-                child: Image(
-                    image: NetworkImage('https://picsum.photos/600/360'))),
-            Text(
-              'Hello, Welcome to my site, I will help you for better than development on flutter dart languages.',
+    return HomeResponsiveChild(
+        child: LayoutBuilder(builder: (context, constraints) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(flex: 4, child: Image(image: NetworkImage(background))),
+          Expanded(
+            child: Text(
+              'Hello, Welcome to my site, I will help you for better than development ıo',
               style: context.headline5,
               textAlign: TextAlign.center,
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       );
-    });
+    }));
   }
+}
+
+class HomeResponsiveChild extends MultiBuilder {
+  HomeResponsiveChild({super.key, required Widget child})
+      : super(builder: (context, height, width, constraints, orientation) {
+          return SizedBox(
+            height:
+                constraints.maxWidth > 800 ? constraints.maxWidth * 0.6 : 480,
+            child: child,
+          );
+        });
 }
